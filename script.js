b1a3987ff53b5bc15add14101ca84744833056c7
Sami BASMAZ <smibsmz@gmail.com>
2025-01-18 00:38:10 +0300

    proje bitti

--- script.js
+++ script.js
@@ -35,24 +35,39 @@ window.onscroll = () => {
     let header = document.querySelector('header');
     header.classList.toggle('sticky', window.scrollY > 100);
 
+
+
     // ! Remove toggle icon and navbar
     menuIcon.classList.remove('fa-xmark');
     navbar.classList.remove('active');
-}
+};
 
 
 
+// ! Scroll reveal
+
+ScrollReveal({
+    distance:'80px',
+    duration: 2000,
+    delay: 200,
+});
+
+ScrollReveal().reveal('.home-content, heading',  {origin: 'top'});
+ScrollReveal().reveal('.home-img, .services-container, portfolio-box, .contact form', { origin: 'buttom'});
+ScrollReveal().reveal('.home-contact h1, .about-img', { origin: 'left'});
+ScrollReveal().reveal('.home-contact p, .about-content', { origin: 'right'});
 
 
 
+// ! Typed Js
 
-
-
-
-
-
-
-
+const typed = new Typed('.multiple-text', {
+    strings: ['Frontend Developer', 'Backend Developer', 'Web Designer', ],
+    typeSpeed: 70,
+    backSpeed: 70,
+    backDelay: 1000,
+    loop: true,
+});
 
 
 
